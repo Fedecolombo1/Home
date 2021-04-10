c8375798e4df2a4ee5c32937ba34f123096779f1
Fedecolombo1 <colombofederico17@gmail.com>
2021-04-10 00:32:07 -0300

    menu hamburguer

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", function(){
+	
+
 	const spans = document.querySelectorAll('.word span');
 
 	spans.forEach((span, idx) => {
@@ -15,4 +17,20 @@ window.addEventListener("load", function(){
 		}, 750 * (idx+1))
 	});
 
+
+
+
+
+
+
+	$("#bt-menu-mobile").click(function(){
+  
+		$(this).toggleClass("close-bt-hamburguer");
+		$(".naranja").toggleClass("abierto");
+		$(".azul").toggleClass("abierto");
+	  
+		  return false;
+		
+	  });
+	  
 })
